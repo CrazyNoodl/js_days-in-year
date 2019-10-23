f3cd9cd805183dbadb6f39925105e2dc8f1b4a49
Rudenko Artem <artem.rud@bk.ru>
2019-10-23 11:33:52 +0300

    fix year

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function daysInYear(year) {
   try {
     if (Number.isInteger(year)) {
       
-      return year % 4 === 0 ? 366 : 365
+      return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0) ? 366: 365;
     }
 
     throw error;
